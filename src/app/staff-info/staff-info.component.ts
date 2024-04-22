6bf9796063c765337b593badb617130e96574fde
Philip Zhang <70548778+philipzhang86@users.noreply.github.com>
2024-04-22 12:15:18 -0400

    goBack() added to staff info page

--- src/app/staff-info/staff-info.component.ts
+++ src/app/staff-info/staff-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AuthService } from '../auth.service'; // 确保你有AuthService来获取token
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-staff-info',
@@ -12,7 +13,7 @@ export class StaffInfoComponent implements OnInit {
   staff: any = {}; // 存储Staff信息
   role: string | null = this.authService.getRole(); // 获取角色
 
-  constructor(private http: HttpClient, private authService: AuthService) { }
+  constructor(private http: HttpClient, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
     this.getStaffInfo(); // 页面初始化时获取Staff信息
@@ -28,5 +29,9 @@ export class StaffInfoComponent implements OnInit {
     const token = this.authService.getToken();
     this.http.put('http://localhost:9080/user-service/api/staffs/', this.staff, { headers: new HttpHeaders({ 'Authorization': `Bearer ${token}` }) })
       .subscribe(() => alert('Staff info updated'), error => console.error('Error updating client info', error));
+  }
+
+  goBack(): void {
+    this.router.navigate(['/staff-dashboard'])
   }
 }
