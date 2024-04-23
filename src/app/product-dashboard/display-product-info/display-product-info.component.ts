709a5fee7e738f7b494df0b84133a715f7ae600b
Philip Zhang <70548778+philipzhang86@users.noreply.github.com>
2024-04-23 16:59:20 -0400

    display-product-info statble

--- src/app/product-dashboard/display-product-info/display-product-info.component.ts
+++ src/app/product-dashboard/display-product-info/display-product-info.component.ts
@@ -9,12 +9,44 @@ import { Router } from '@angular/router';
   styleUrls: ['./display-product-info.component.scss']
 })
 export class DisplayProductInfoComponent implements OnInit {
+  search = {
+    id: '',
+    name: '',
+    sku: ''
+  };
+  product: any;
+
+
 
   constructor(
-    private router: Router
+    private router: Router,
+    private http: HttpClient,
+    private authService: AuthService
   ) { }
 
   ngOnInit(): void {
+  }
+
+  searchProduct(form: any): void {
+    let url = 'http://localhost:9081/asn-service/api/clients/products/';
+    const token = this.authService.getToken();
+    const headers = new HttpHeaders({ 'Authorization': `Bearer ${token}` });
+
+    if (this.search.id) {
+      url += `by-id/${this.search.id}`;
+    } else if (this.search.name) {
+      url += `by-name/${this.search.name}`;
+    } else if (this.search.sku) {
+      url += `by-sku/${this.search.sku}`;
+    }
+
+    this.http.get<any>(url, { headers }).subscribe(
+      product => this.product = product,
+      error => {
+        console.error('Error fetching product info', error);
+        alert('Failed to fetch product info');
+      }
+    );
   }
 
   goBack(): void {
