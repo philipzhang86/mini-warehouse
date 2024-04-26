06842fede58a6a964d4f325382c155436ef258ed
Philip Zhang <70548778+philipzhang86@users.noreply.github.com>
2024-04-26 15:56:01 -0400

    update-asn-item 1.0 added

--- src/app/asn-item-dashboard/update-asn-item/update-asn-item.component.ts
+++ src/app/asn-item-dashboard/update-asn-item/update-asn-item.component.ts
@@ -9,6 +9,13 @@ import { AuthService } from 'src/app/auth.service';
   styleUrls: ['./update-asn-item.component.scss']
 })
 export class UpdateAsnItemComponent implements OnInit {
+  searchAsnItem = {
+    id: '',
+    asnId: '',
+    sku: ''
+  };
+  asnItem: any = {};
+
 
   constructor(
     private http: HttpClient,
@@ -19,7 +26,44 @@ export class UpdateAsnItemComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  findAsnItem(): void {
+    let url = 'http://localhost:9081/asn-service/api/clients/asn-items/';
+    const token = this.authService.getToken();
+    const headers = new HttpHeaders({ 'Authorization': `Bearer ${token}` });
+
+    if (this.searchAsnItem.id) {
+      url += `by-id/${this.searchAsnItem.id}`;
+    } else if (this.searchAsnItem.asnId && this.searchAsnItem.sku) {
+      url += `by-asn-id-and-sku/${this.searchAsnItem.asnId}/${this.searchAsnItem.sku}`;
+    } else {
+      alert('Please provide both ASN ID and SKU or an ASN Item ID');
+      return;
+    }
+
+    this.http.get<any>(url, { headers }).subscribe(
+      asnItem => {
+        this.asnItem = asnItem;
+        console.log(this.asnItem);
+      },
+      error => {
+        console.error('Error fetching asn item info', error);
+        alert('Failed to fetch asn item info');
+      }
+    );
+  }
+
+  updateAsnItem(): void {
+    const token = this.authService.getToken();
+    this.http.put('http://localhost:9081/asn-service/api/clients/asn-items/', this.asnItem, {
+      headers: new HttpHeaders({ 'Authorization': `Bearer ${token}` })
+    }).subscribe(
+      () => alert('ASN item info updated'),
+      error => console.error('Error updating ASN item info', error)
+    );
+  }
+
   goBack(): void {
     this.router.navigate(['/client-dashboard/asn-operations-center/asn-item-dashboard']);
   }
 }
+
